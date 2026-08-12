9a4f65f20743be3446d4a457245c41147cc3f9bb
Reza Nadimi <rezaxr14@gmail.com>
2026-08-12 14:52:32 +0330

    chore: clean up root directory scratchpads and patches

--- debug_dentex.py
+++
@@ -1,9 +0,0 @@
-from dental_agent.data.dentex import discover_annotation_files
-import glob
-
-# Try loading all
-all_coco = discover_annotation_files("data")
-print(f"Total loaded: {len(all_coco)}")
-for k in all_coco.keys():
-    if "train_quadrant" in k:
-        print("Found:", k)
